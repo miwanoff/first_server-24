ce6928d8dc8424624f5949ddadba5cd478f029d9
miwanoff <marina.iva.noffa@gmail.com>
2024-05-04 13:19:50 +0300

    Add nodemon

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-let http = require("http");
-let dt = require('./myfirstmodule');
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-	res.write("Time:" + dt.myDateTime());
-	res.end();
-  })
-  .listen(8080);
+const express = require("express");
+const app = express();
+const port = 8080;
+//app.use(express.static(__dirname));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/1", function (req, res) {
+  res.sendFile(__dirname + "/winter.html");
+});
+app.get("/2", function (req, res) {
+  res.sendFile(__dirname + "/summer.html");
+});
+app.get("/3", function (req, res) {
+    res.sendFile(__dirname + "/index1.html");
+  });
+app.listen(port, function () {
+  return console.log("port is " + port);
+});
